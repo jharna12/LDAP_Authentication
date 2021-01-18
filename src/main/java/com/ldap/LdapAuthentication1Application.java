112abe8f286e6b4303052a071f91dd63572f9a27
Jharna Agrawal <jagrawal@BLR.ALLEGISINDIA.COM>
2021-01-18 18:10:51 +0530

    LDAP Authentication using JAVA

---
+++ src/main/java/com/ldap/LdapAuthentication1Application.java
@@ -0,0 +1,13 @@
+package com.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapAuthentication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapAuthentication1Application.class, args);
+	}
+
+}
